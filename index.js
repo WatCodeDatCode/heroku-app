9af06bcff939b6d0bbab33838a4c6083008ce483
WatCodeDatCode <arussell742@gmail.com>
2020-10-28 20:17:51 +0100

    Add test endpoint and mysql2

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 require('dotenv').config();
+
 const express = require('express');
+const mysql = require('mysql2');
 const app = express();
 
 const port = process.env.PORT;
 
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
+})
+
 app.get('/', (req, res) => {
     res.send('It works!');
+});
+
+app.get('/dbtest', (req, res) => {
+    connection.query(`SELECT * FROM offices`, (error, result) => {
+        res.json(result);
+    })
 });
 
 app.listen(port, () => {
