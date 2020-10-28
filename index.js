e1821688a98f87d9f5c66021b0b768d83bd0cdab
WatCodeDatCode <arussell742@gmail.com>
2020-10-28 20:56:40 +0100

    Add initial database queries

--- index.js
+++ index.js
@@ -11,16 +11,56 @@ const connection = mysql.createConnection({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
-})
+});
 
 app.get('/', (req, res) => {
     res.send('It works!');
 });
 
-app.get('/dbtest', (req, res) => {
-    connection.query(`SELECT * FROM offices`, (error, result) => {
+app.get('/api/employees', (req, res) => {
+    connection.query(`SELECT * FROM employees`, (error, result) => {
+
         res.json(result);
+        
     })
+});
+
+app.get('/api/employee/:id', (req, res) => {
+
+    const id = req.params.id;
+    console.log(id);
+
+    connection.query(`SELECT * FROM employees WHERE employeeNumber = ?`, [id], (error, result) => {
+        
+        if (error) {
+            res.json({
+                status: 'error',
+                description: 'error, try again later'
+            });
+        }
+
+        res.json(result);
+
+    });
+});
+
+app.get('/api/office/:id', (req, res) => {
+
+    const id = req.params.id;
+    console.log(id);
+
+    connection.query(`SELECT * FROM offices WHERE officeCode = ?`, [id], (error, result) => {
+        
+        if (error) {
+            res.json({
+                status: 'error',
+                description: 'error, try again later'
+            });
+        }
+
+        res.json(result);
+
+    });
 });
 
 app.listen(port, () => {
